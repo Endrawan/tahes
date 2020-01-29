5ef96d55082f1a80d9f90dbce7835d91f38caca2
Endrawan <nadirahmat@gmail.com>
2020-01-29 13:01:27 +0700

    Create Profile and Change image to the correct one

--- app/src/main/java/com/endrawan/tahes/ui/menu/ProfileFragment.kt
+++ app/src/main/java/com/endrawan/tahes/ui/menu/ProfileFragment.kt
@@ -2,24 +2,35 @@ package com.endrawan.tahes.ui.menu
 
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.endrawan.tahes.R
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class ProfileFragment : Fragment() {
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Glide.with(this)
+            .load(R.drawable.stat1)
+            .centerCrop()
+            .into(stat1)
+
+        Glide.with(this)
+            .load(R.drawable.stat2)
+            .centerCrop()
+            .into(stat2)
+
+        Glide.with(this)
+            .load(R.drawable.header)
+            .centerCrop()
+            .into(header_image)
+
     }
-
 
 }
--- app/src/main/java/com/endrawan/tahes/ui/sign_up/Page4Fragment.kt
+++ app/src/main/java/com/endrawan/tahes/ui/sign_up/Page4Fragment.kt
@@ -2,24 +2,24 @@ package com.endrawan.tahes.ui.sign_up
 
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.endrawan.tahes.R
+import kotlinx.android.synthetic.main.fragment_page4.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class Page4Fragment : Fragment() {
+class Page4Fragment : Fragment(R.layout.fragment_page4) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_page4, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Glide.with(this)
+            .load(R.drawable.welcome)
+            .fitCenter()
+            .into(image)
     }
-
 
 }
